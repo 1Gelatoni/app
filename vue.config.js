a3c2786b7da8136d21cffe23f26c10c19525abdd
1Gelatoni <115600221+1Gelatoni@users.noreply.github.com>
2023-04-11 17:31:03 +0800

    Add files via upload

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,18 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  lintOnSave:false,
+  devServer: {
+    open: true,
+    host: 'localhost',
+    port: 8080,
+    // 代理跨域
+    proxy:{
+      '/api':{   //请求路径中带有'/api' 代理服务器工作
+        target:"http://gmall-h5-api.atguigu.cn ",
+        // pathRewrite:{'^/api/':''}
+      }
+    }
+  },
 })
+
